8fded0da0a7906cd9166c5ef1a3faed649c0e6cd
katchenin-13 <katchenin03@gmail.com>
2023-12-22 08:59:29 +0530

    JDBC Authentication

--- src/main/java/ma/enset/hospitalapp/HospitalAppApplication.java
+++ src/main/java/ma/enset/hospitalapp/HospitalAppApplication.java
@@ -6,7 +6,13 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 
+import javax.sql.DataSource;
 import java.util.Date;
 
 @SpringBootApplication
@@ -24,4 +30,29 @@ public class HospitalAppApplication {
             patientRepository.save(new Patient(null,"Laila",new Date(),false,123));
         };
     }
+
+    @Bean
+    PasswordEncoder passwordEncoder(){
+
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner(JdbcUserDetailsManager jdbcUserDetailsManager){
+       PasswordEncoder passwordEncoder = passwordEncoder();
+       return args -> {
+           UserDetails u1 = jdbcUserDetailsManager.loadUserByUsername("user11");
+         if (u1 == null)
+             jdbcUserDetailsManager.createUser(
+                     User.withUsername("user11").password(passwordEncoder.encode("1234")).roles("USER").build()
+             );
+         UserDetails u2 = jdbcUserDetailsManager.loadUserByUsername("admin1");
+         if (u2 == null)
+            jdbcUserDetailsManager.createUser(
+                    User.withUsername("admin1").password(passwordEncoder.encode("1234")).roles("USER","ADMIN").build()
+
+            );
+       };
+    }
+
 }
